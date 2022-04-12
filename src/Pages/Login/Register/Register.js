f13ec029b2463fe89a1dfa8a1156a7ec7796b340
AbuZobayed <zobayed.ict05@gmail.com>
2022-04-12 23:27:56 +0600

    register page desgin and toggle login to register

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Register.css'
 
 const Register = () => {
+    const navigate = useNavigate();
+
+    const navigateLogin = () => {
+        navigate('/login');
+    }
+
     return (
-        <div>
-            <h2>please resiter</h2>
+        <div className='register-form'>
+            <h2 style={{textAlign:'center'}}>Please Register</h2>
+            <form>
+                <input type="text" name="name" id="" placeholder='Your name'/>
+              
+                <input type="email" name="" id="" placeholder='Email Address' required/>
+               
+                <input type="password" name="" id="" placeholder='Password' required/>
+
+                <input type="submit" value="Register" />
+            </form>
+
+            <p>Already have an Account? <Link to="/login" className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link> </p>
         </div>
     );
 };
